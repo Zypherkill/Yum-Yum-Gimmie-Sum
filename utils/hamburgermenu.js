6050937d4cd49a15ae3c494eeec2e2f997d08f07
Hannes Bäcklund <3kdyhfe24.backha@folkuniversitetet.nu>
2025-03-17 16:33:38 +0100

    Styling

--- utils/hamburgermenu.js
+++ utils/hamburgermenu.js
@@ -2,15 +2,24 @@ export function showNavigation () {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const slideMenu = document.querySelector('.navigation');
     const closeButton = document.querySelector('.fa-xmark');
-    const contentWrapper = document.querySelector('.about-us');
+    const contentWrapper = document.querySelector('.content-wrapper');
+    const menuWrapper = document.querySelector('.menu-wrapper');
     
     hamburgerMenu.addEventListener('click', () => {
         slideMenu.classList.add('open');
-        contentWrapper.classList.add('blur');
+        if (window.location.pathname === ("/pages/meny.html")) {
+            menuWrapper.classList.add('blur');
+        } else {
+            contentWrapper.classList.add('blur');
+        }
     });
     
     closeButton.addEventListener('click', () => {
         slideMenu.classList.remove('open');
-        contentWrapper.classList.remove('blur');
+        if (window.location.pathname === ("/pages/meny.html")) {
+            menuWrapper.classList.remove('blur');
+        } else {
+            contentWrapper.classList.remove('blur');
+        }
     });
     }
